b9619dd83617cd4768bd027bdf4095cd67a90e8b
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-01-09 20:18:41 -0400

    feat: install LineIcons

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -4,13 +4,13 @@ import './profile.css';
 const Profile = () => {
     return (
     <div className="profile">
-        <img src="../assets/imgs/avatarProfile.jpg" alt="avatar" />
+        <img src="/public/imgs/avatarProfile.jpg" alt="avatar" />
         <div>
             <h2>Nombre</h2>
             <h3>Carrera</h3>
             <p>Description</p>
             <div className="profile-socials">
-                <button><i className="fa-brands fa-dribbble"></i></button>
+                <button><i className="lni lni-instagram"></i></button>
                 <button><i className="fa-brands fa-pinterest"></i></button>
                 <button><i className="fa-brands fa-codepen"></i></button>
             </div>
