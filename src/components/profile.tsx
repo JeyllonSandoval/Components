88b7134d462e2511198c2b30dc76138f3785ea2b
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-01-09 20:56:10 -0400

    feat: profile panel

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,13 +6,17 @@ const Profile = () => {
     <div className="profile">
         <img src="/public/imgs/avatarProfile.jpg" alt="avatar" />
         <div>
-            <h2>Nombre</h2>
-            <h3>Carrera</h3>
-            <p>Description</p>
+            <h2>Jeyllon Sandoval</h2>
+            <h3>Systems Engineer</h3>
+            <p>I am a Systems Engineering student with a diverse set of technical and interpersonal skills. 
+                I am a native Spanish speaker and possess a basic proficiency in English, 
+                which I continuously strive to improve. 
+                My academic background and personal interests have allowed me to develop a strong foundation 
+                in problem-solving, programming, and analytical thinking.</p>
             <div className="profile-socials">
                 <button><i className="lni lni-instagram"></i></button>
-                <button><i className="fa-brands fa-pinterest"></i></button>
-                <button><i className="fa-brands fa-codepen"></i></button>
+                <button><i className="lni lni-linkedin"></i></button>
+                <button><i className="lni lni-github"></i></button>
             </div>
         </div>
     </div>
